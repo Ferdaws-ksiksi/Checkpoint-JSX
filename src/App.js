ccef77d75ea57930f209eb5b7c3a6dc55a57d857
Ferdaws-ksiksi <85398562+Ferdaws-ksiksi@users.noreply.github.com>
2021-07-23 11:46:02 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+// import './App.css';
+import imagesrc from "./imagesrc.png"
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="titel red">
+       Ferdaws
+     </h1>
+     <div>
+       <br/>
+     </div>
+     <img src={imagesrc} alt='image'/>
+     <div>
+       <br/>
+     </div>
+     <img src='/imagepub.png'/>
+     <div>
+     <iframe width="320" height="240" src="https://www.youtube.com/embed/yTNE1ru9ELc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     </div>
+    </div>
+  );
+}
+
+export default App;
